ad601dc4f26bfca050a55fa6d92513809c5fdb77
GuiZeroUm <guilherme2004official@hotmail.com>
2024-06-09 13:17:01 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Guilherme Santos on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
